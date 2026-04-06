c98a8ce0d4066d6185c77b0d0a175237a52b8003
ef3d0c3e <ef3d0c3e@pundalik.org>
2026-04-06 02:45:37 +0200

    License

--- src/db.rs
+++ src/db.rs
@@ -1,3 +1,24 @@
+/*
+* db.rs
+*
+* nvpass -- Neovim secure vault
+* Copyright (C) 2026 ef3d0c3e <ef3d0c3e@pundalik.org>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* SPDX-License-Identifier: GPL-3.0-or-later
+*/
 use std::io::{self, Read, Write};
 
 use serde::{Deserialize, Serialize};
